87fffef9e73f3a6b89b4ff985449c660e24df403
Omar044 <crisomarrios@gmail.com>
2024-10-19 12:00:03 -0500

    a

--- PRACTICA07_DA/Experiecnia 02/Exp02_Ejemplo01/pages/Players.js
+++ PRACTICA07_DA/Experiecnia 02/Exp02_Ejemplo01/pages/Players.js
@@ -1,16 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
+import { usePlayer } from "../context/PlayerContext"; // Importa el contexto
 
 const Players = () => {
+  const { players } = usePlayer(); // Obtiene la lista de jugadores del contexto
+
   return (
     <div>
       <h1>Jugadores del FC Barcelona</h1>
       <ul>
-        <li>
-          <Link to="1">Robert Lewandowski</Link>
-        </li>
-        <li>
-          <Link to="2">Lamine Yamal</Link>
-        </li>
+        {players.map(player => ( // Mapea la lista de jugadores
+          <li key={player.id}>
+            <Link to={player.id.toString()}>{player.name}</Link>
+          </li>
+        ))}
       </ul>
       <Outlet /> 
     </div>
